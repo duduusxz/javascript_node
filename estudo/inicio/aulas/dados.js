c0a72c7e56558d6f1f8f90d2685d55fcdd8706de
duduusxz <eduardoataidemelo@gmail.com>
2025-05-23 22:27:00 -0300

    Começando com variáveis e dados

---
+++ estudo/inicio/aulas/dados.js
@@ -0,0 +1,15 @@
+let nome = "Canetas"
+let quantia = 10
+let preco = 6
+let imposto = 1.5
+let precoFinal = preco + imposto
+
+console.log(nome)
+console.log(quantia)
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+// Posso alterar a variável ao decorrer do código
+nome = "Caneta azul"
+console.log(nome)
