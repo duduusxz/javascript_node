96c24cb5a4aeac80c51469f8897cfbe9be93bc6b
duduusxz <eduardoataidemelo@gmail.com>
2025-05-23 22:08:15 -0300

    Aprendendo sentenças e blocos de códigos

---
+++ estudo/inicio/bloco_codigo.js
@@ -0,0 +1,12 @@
+{
+    console.log("Bloco de código1")
+    console.log("Bloco de código2")
+    console.log("Bloco de código3")
+}
+
+{
+    console.log("Bloco de código4")
+    console.log("Bloco de código5")
+}
+
+console.log("Final")
---
+++ estudo/inicio/sentenca_codigo.js
@@ -0,0 +1,8 @@
+// Um bloco é o agrupamento de sentenças de código
+// Uma sentença de código pode terminar com ou sem ponte e vírgula
+// Uma sentença de código é um comnando que o computador vai executar.
+
+console.log("Olá")
+console.log("Hello Word")
+
+// console.log é um comando que será exibido na tela do computador
