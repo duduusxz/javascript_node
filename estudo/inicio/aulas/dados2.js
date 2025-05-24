6e369bc9cb42e8d9b2a1a650ab13e06e695f36cb
duduusxz <eduardoataidemelo@gmail.com>
2025-05-23 23:52:05 -0300

    estudando variaveis e dados

---
+++ estudo/inicio/aulas/dados2.js
@@ -0,0 +1,11 @@
+console.log(15.90 * 0.5)
+
+let preco = 15.90
+let desconto = 0.5
+let precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto)
+
+let nome = "Carne" // String (texto) = sequencia de símbolos
+let categoria = "Açougue"
+
+console.log("Produto: " + nome + ", Categoria: " + categoria + ", Preço: " + preco + ", Desconto: " + desconto + ", Preço Final: " + precoComDesconto)
---
+++ estudo/inicio/aulas/dados3.js
@@ -0,0 +1,12 @@
+let idade = 31
+
+console.log(typeof -31)
+console.log(typeof idade)
+
+let salario = 4567.98
+console.log(typeof salario)
+
+let estaChovendo = false
+console.log(typeof estaChovendo)
+
+console.log(typeof "teste")
