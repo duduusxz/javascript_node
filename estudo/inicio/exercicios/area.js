c5036be848d23588f9c1876edb9bedd3d0aab300
duduusxz <eduardoataidemelo@gmail.com>
2025-05-25 22:57:53 -0300

    Fazendo alguns exercícios de variáveis

---
+++ estudo/inicio/exercicios/area.js
@@ -0,0 +1,9 @@
+// PI * r * r
+const PI = 3.1415
+let raio = 20
+let areaCircunferencia = PI * raio * raio
+
+console.log("A área da cincunferência é: " + areaCircunferencia + "m²")
+
+areaCircunferencia = Math.PI * raio * raio
+console.log("A área da cincunferência é: " + areaCircunferencia + "m²")
