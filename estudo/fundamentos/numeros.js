869d0fc3074bcf1f30e3f794f51fb696de562eb4
duduusxz <eduardoataidemelo@gmail.com>
2025-05-26 18:49:20 -0300

    Entendendo tipagem JS e alguns cuidados que temos que ter

---
+++ estudo/fundamentos/numeros.js
@@ -0,0 +1,33 @@
+/*{
+    const peso1 = 1.3
+    const peso2 = Number('2.0')
+
+    console.log(peso1, peso2)
+    .log(Number.isInteger(peso1))
+    console.log(Number.isInteger(peso2))
+
+    const nota1 = 8.212
+    const nota2 = 7.23
+
+    const total = nota1 * peso1 + nota2 * peso2
+    const media = total / (peso1 + peso2)
+
+    console.log(media.toFixed(2)) // aqui mostra apenas 2 casas decimais
+    console.log(media.toString(2)) // aqui está em número binário 
+    console.log(typeof media)
+    console.log(typeof Number)
+}
+*/
+
+{
+    console.log(7 / 0)
+    console.log("10" / 2)
+    console.log(0.1 + 0.5)
+
+    let a = 10
+
+    console.log(typeof a.toString())
+    console.log(typeof a)
+
+    console.log(typeof(10).toString())
+}
---
+++ estudo/fundamentos/tipagem.js
@@ -0,0 +1,11 @@
+let a = 'legal'
+console.log(a)
+console.log(typeof a)
+
+a = 3.2
+console.log(a)
+console.log(typeof a)
+
+let valor = ''
+let num = 1 //não usar nomes genéricos
+let blz = '' //não utilizar siglas
