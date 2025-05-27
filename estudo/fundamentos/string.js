4f1c36b1b7e976cce9bb2e7e770fd4f06267e54c
duduusxz <eduardoataidemelo@gmail.com>
2025-05-26 23:45:36 -0300

    Mexendo com strings e strings template

---
+++ estudo/fundamentos/string.js
@@ -0,0 +1,16 @@
+const escola = "ET3C"
+let a = 4
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(3))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('C'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log("Escola ".concat(escola.concat("!")))
+console.log("Você tirou a nota: ".concat(a).concat(" prova de historia"))
+console.log(escola.replace('3', 'E'))
+
+console.log('Ana,Maria,Pedro'.split(','))
